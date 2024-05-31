9f7b460bfc7e00b4b575e7ccb48072738b7175ca
Henrique Ornelas <rickornelas.ho@gmail.com>
2024-05-31 17:55:25 -0300

    Ajustes de Bundle

--- dist/ornelas-dev/server/main.server.mjs
+++ dist/ornelas-dev/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BOO6J7FW.mjs";import"./chunk-QJZ5UKEU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-QOEBDOW4.mjs";import"./chunk-QJZ5UKEU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
